e41e8abfed38c125755a36472f07ebecd63910a2
absaaraslam <absaaraslam123@gmail.com>
2023-06-14 01:50:24 +0500

    initial commit

---
+++ src/components/Banner.js
@@ -0,0 +1,127 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import headerImg from "../assets/img/header-img.svg";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+import axios from 'axios';
+
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
+  const period = 2000;
+  const [debt, setDebt] = useState(null);
+
+  // useEffect(() => {
+  //   const fetchDebtNumbers = async () => {
+  //     try {
+  //       const response = await axios.get(
+  //         'https://cors-anywhere.herokuapp.com/https://www.usadebtclock.com/api/json',
+  //         {
+  //           headers: {
+  //             'X-Requested-With': 'XMLHttpRequest',
+  //           },
+  //         }
+  //       );
+  //       setDebt(response.data.USDebt);
+  //     } catch (error) {
+  //       console.error('Error fetching debt numbers:', error);
+  //     }
+  //   };
+
+  //   fetchDebtNumbers();
+  // }, []);
+
+  // useEffect(() => {
+  //   let ticker = setInterval(() => {
+  //     tick();
+  //   }, delta);
+
+  //   return () => { clearInterval(ticker) };
+  // }, [text])
+
+  // const tick = () => {
+  //   let i = loopNum % toRotate.length;
+  //   let fullText = toRotate[i];
+  //   let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+  //   setText(updatedText);
+
+  //   if (isDeleting) {
+  //     setDelta(prevDelta => prevDelta / 2);
+  //   }
+
+  //   if (!isDeleting && updatedText === fullText) {
+  //     setIsDeleting(true);
+  //     setIndex(prevIndex => prevIndex - 1);
+  //     setDelta(period);
+  //   } else if (isDeleting && updatedText === '') {
+  //     setIsDeleting(false);
+  //     setLoopNum(loopNum + 1);
+  //     setIndex(1);
+  //     setDelta(500);
+  //   } else {
+  //     setIndex(prevIndex => prevIndex + 1);
+  //   }
+  // }
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="align-items-center">
+     
+          <Col xs={12} md={6} xl={7} className="text-center text-md-start" >
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                  <span className="tagline">Introduction</span>
+                  <h1>{`Hi! this is Flipscape.`}
+                    {/* <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span> */}
+                  </h1>
+                  {/* <div>
+                    <h2>United States National Debt</h2>
+                    {debt ? (
+                      <p>{debt.toLocaleString('en-US')}</p>
+                    ) : (
+                      <p>Loading...</p>
+                    )}
+                  </div> */}
+
+
+                  <p>In its humble beginnings, this was a blog about my day to day life, and has now become a news blog about what is going on in America and the world. I hope you can enjoy the rss news feed here, as well as my commentary. I'll try to always add some wit, psychology, and philosophy for life. I find that I learn something new everyday, so why not keep track of it. Everything happens for a reason and I like to think about that reason and the affect to truly consider the nature of whats going on.</p>
+                 
+                  <button onClick={() => console.log('connect')}>See My Blogs. <ArrowRightCircle size={25} /></button>
+                </div>}
+            </TrackVisibility>
+          </Col>
+        
+          <Col xs={12} md={8} xl={5}  >
+            <TrackVisibility>
+              {({ isVisible }) =>
+                // <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                //   <img src={headerImg} alt="Header Img" />
+                // </div>
+              
+                  <div className="col-12 justify-content-center">
+                    <div className="debt-clock wow zoomIn">
+                      <h2>U.S National Debt</h2>
+                      <p>Live Debt Clock<br></br></p>
+                      {//Debt clock component
+                      }
+                      <iframe src="http://www.USADebtClock.com/us-debt-clock-widget.php" height="100" width="350" scrolling="no" frameborder="0">
+                      </iframe>
+                    </div>
+                  </div>
+               
+              }
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
---
+++ src/components/Projects.js
@@ -0,0 +1,139 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+import Carousel from 'react-multi-carousel';
+import meter1 from "../assets/img/meter1.svg";
+import meter2 from "../assets/img/meter2.svg";
+import meter3 from "../assets/img/meter3.svg";
+import meter from "../assets/img/meter.png";
+
+export const Projects = () => {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
+
+  const projects = [
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+  ];
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                  <h2>News And Blogs</h2>
+                  <p>Here are some of the blogs I have written, hope you all enjoy.</p>
+                  <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
+          <div className="item">
+            <img src={meter} alt="Image" />
+            <h5>Blog1</h5>
+          </div>
+          <div className="item">
+            <img src={meter} alt="Image" />
+            <h5>Blog2</h5>
+          </div>
+          <div className="item">
+            <img src={meter} alt="Image" />
+            <h5>Blog3</h5>
+          </div>
+          <div className="item">
+            <img src={meter} alt="Image" />
+            <h5>Blog4</h5>
+          </div>
+        </Carousel>
+                  {/* <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                      <Nav.Item>
+                        <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                    <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                      <Tab.Pane eventKey="first">
+                        <Row>
+                          {
+                            projects.map((project, index) => {
+                              return (
+                                <ProjectCard
+                                  key={index}
+                                  {...project}
+                                />
+                              )
+                            })
+                          }
+                        </Row>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="section">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="third">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container> */}
+                </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
